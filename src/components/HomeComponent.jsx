842dd945123dc88000501d7b6bdaef1abebfd00d
Ramen Biswas <ramen@devsramen.com>
2025-04-22 19:55:13 +0600

    Update Pause & Resume button at 22-04-2025_1955.

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -6,22 +6,23 @@ const HomeComponent = () => {
     const CELL_SIZE = 20;
     const INITIAL_SNAKE = [
         {
-            x : 10,
+            x : 1,
             y : 10
         }
     ];
-    const INITIAL_FOOD = [
+    const INITIAL_FOOD =
         {
             x : 15,
             y : 15
         }
-    ];
+    ;
     const INITIAL_DIRECTION = 'RIGHT';
     const [ snake, setSnake] = useState(INITIAL_SNAKE);
     const [ food, setFood] = useState(INITIAL_FOOD);
     const [ direction, setDirection] = useState(INITIAL_DIRECTION);
     const [ score, setScore] = useState(0);
     const [ gameOver, setGameOver] = useState(false);
+    const [ gamePause, setGamePause] = useState(false);
 
     const generateFood = ()=>{
         const newFood = {
@@ -31,118 +32,127 @@ const HomeComponent = () => {
         return snake.some((item) => item.x === newFood.x && item.y === newFood.y) ? generateFood() : newFood;
     }
 
-const moveSnake = ()=>{
-    if(gameOver) return;
+    const moveSnake = ()=>{
+        if(gameOver || gamePause) return;
 
-    const head = { ...snake[0] };
+        const head = { ...snake[0] };
 
-    switch (direction) {
-        case "UP":
-            head.y--;
-            break;
-        case "DOWN":
-            head.y++;
-            break;
-        case "LEFT":
-            head.x--;
-            break;
-        case "RIGHT":
-            head.x++;
-            break;        
-        default:
-            break;
-    }
-
-    if( head.x < 0 ||
-        head.x >= GRID_SIZE ||
-        head.y < 0 ||
-        head.y >= GRID_SIZE ||
-        snake.some((item)=>item.x === head.x && item.y === head.y)
-    ){
-            // setGameOver(true);
-            return;
-    }
-
-    const newSnake = [head, ...snake]; 
-    if(head.x === food.x && head.y === food.y){
-        setScore(score + 1);
-        setFood(generateFood());
-    }else{
-        newSnake.pop();
-    }
-    setSnake(newSnake)
-}
-
-useEffect(()=>{
-    const handleKeyPress = (e) => {
-        switch (e.key) {
-            case "ArrowUp":
-                if( direction !== "DOWN") setDirection("UP")
+        switch (direction) {
+            case "UP":
+                head.y--;
                 break;
-            case "ArrowDown":
-                if( direction !== "UP") setDirection("DOWN")
+            case "DOWN":
+                head.y++;
                 break;
-            case "ArrowLeft":
-                if( direction !== "RIGHT") setDirection("LEFT")
+            case "LEFT":
+                head.x--;
                 break;
-            case "ArrowRight":
-                if( direction !== "LEFT") setDirection("RIGHT")
-                break;
-        
+            case "RIGHT":
+                head.x++;
+                break;        
             default:
                 break;
         }
-    }
-    window.addEventListener("keydown",handleKeyPress)
-    return () => {
-        window.removeEventListener("keydown",handleKeyPress)
-    };
-},[direction])
 
-useEffect(() => {
-    const gameLoop = setInterval(moveSnake, 300)
-    return () => {
-        clearInterval(gameLoop);
-    };
-},[snake, direction, gameOver])
-
-const renderGrid = () => {
-    const grid = [];
-    for( let i = 0; i < GRID_SIZE; i++){
-        for( let j =0; j < GRID_SIZE; j++){
-            const isSnake = snake.some((item)=> item.x === j && item.y === i);
-            const isFood = food.x === j && food.y === i;
-            grid.push(
-                <div key={`${i}-${j}`} className={`cell ${isSnake ? "snake" : ""} ${isFood ? "food" : ""}`}>
-
-                </div>
-            )
+        if( head.x < 0 ||
+            head.x >= GRID_SIZE ||
+            head.y < 0 ||
+            head.y >= GRID_SIZE ||
+            snake.some((item)=>item.x === head.x && item.y === head.y)
+        ){
+                // setGameOver(true);
+                return;
         }
+
+        const newSnake = [head, ...snake]; 
+        if(head.x === food.x && head.y === food.y){
+            setScore(score + 1);
+            setFood(generateFood());
+        }else{
+            newSnake.pop();
+        }
+        setSnake(newSnake)
     }
-    return grid;
-}
 
-let resetGame = () => {
-    setSnake(INITIAL_SNAKE)
-    setFood(INITIAL_FOOD)
-    setDirection(INITIAL_DIRECTION)
-    setScore(0)
-    setGameOver(false)
-}
+    useEffect(()=>{
+        const handleKeyPress = (e) => {
+            switch (e.key) {
+                case "ArrowUp":
+                    if( direction !== "DOWN") setDirection("UP")
+                    break;
+                case "ArrowDown":
+                    if( direction !== "UP") setDirection("DOWN")
+                    break;
+                case "ArrowLeft":
+                    if( direction !== "RIGHT") setDirection("LEFT")
+                    break;
+                case "ArrowRight":
+                    if( direction !== "LEFT") setDirection("RIGHT")
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        window.addEventListener("keydown",handleKeyPress)
+        return () => {
+            window.removeEventListener("keydown",handleKeyPress)
+        };
+    },[direction])
 
-// 01:00:00
+    useEffect(() => {
+        const gameLoop = setInterval(moveSnake, 200)
+        return () => {
+            clearInterval(gameLoop);
+        };
+    },[snake, direction, gameOver, gamePause])
 
+    const renderGrid = () => {
+        const grid = [];
+        for( let i = 0; i < GRID_SIZE; i++){
+            for( let j =0; j < GRID_SIZE; j++){
+                const isSnake = snake.some((item)=> item.x === j && item.y === i);
+                const isFood = food.x === j && food.y === i;
+                grid.push(
+                    <div key={`${i}-${j}`} className={`cell ${isSnake ? "snake" : ""} ${isFood ? "food" : ""}`}>
+
+                    </div>
+                )
+            }
+        }
+        return grid;
+    }
+
+    let pauseGame = () => {
+        setGamePause(true)
+    }
+    let resumeGame = () => {
+        setGamePause(false)
+    }
+    let resetGame = () => {
+        setSnake(INITIAL_SNAKE)
+        setFood(INITIAL_FOOD)
+        setDirection(INITIAL_DIRECTION)
+        setScore(0)
+        setGameOver(false)
+    }
 
     return (
         <>
             <div className="game-container">
-                <div className="score">Score: 5</div>
+                <div className="score">Score: {score}</div>
                 <div className="game-board overflow-hidden rounded-md" style={{ gridTemplateColumns: `repeat(${GRID_SIZE}, ${CELL_SIZE}px)`, width: `${GRID_SIZE * CELL_SIZE + 6}px`, height: `${GRID_SIZE * CELL_SIZE  + 5}px`}}>
-                    <div className="food"></div>
                     {
                         renderGrid()
                     }
                 </div>
+                {
+                    gamePause ?
+                    <button className='pauseBtn' onClick={()=>resumeGame()}>Resume</button>
+                    :
+                    <button className='pauseBtn' onClick={()=>pauseGame()}>Pause</button>
+
+                }
                 {
                     gameOver ?
                     <div className="game-over">
